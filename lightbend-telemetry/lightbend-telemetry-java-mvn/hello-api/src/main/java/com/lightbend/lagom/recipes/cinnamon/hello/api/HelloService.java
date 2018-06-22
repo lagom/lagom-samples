5ecc406c8eab8fb16ce56670ae8458467fe26690
Tim Moore <tim.moore@lightbend.com>
2018-06-22 14:53:01 +0930

    Minimize the service code
    This synchronizes with the Java/sbt example documented in
    https://developer.lightbend.com/docs/cinnamon/current/getting-started/lagom_java.html

--- lightbend-telemetry/lightbend-telemetry-java-mvn/hello-api/src/main/java/com/lightbend/lagom/recipes/cinnamon/hello/api/HelloService.java
+++ lightbend-telemetry/lightbend-telemetry-java-mvn/hello-api/src/main/java/com/lightbend/lagom/recipes/cinnamon/hello/api/HelloService.java
@@ -1,21 +1,13 @@
 package com.lightbend.lagom.recipes.cinnamon.hello.api;
 
-import akka.Done;
 import akka.NotUsed;
 import com.lightbend.lagom.javadsl.api.Descriptor;
 import com.lightbend.lagom.javadsl.api.Service;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
-import com.lightbend.lagom.javadsl.api.broker.Topic;
-import com.lightbend.lagom.javadsl.api.broker.kafka.KafkaProperties;
 
-import static com.lightbend.lagom.javadsl.api.Service.*;
+import static com.lightbend.lagom.javadsl.api.Service.named;
+import static com.lightbend.lagom.javadsl.api.Service.pathCall;
 
-/**
- * The Hello service interface.
- * <p>
- * This describes everything that Lagom needs to know about how to serve and
- * consume the Hello.
- */
 public interface HelloService extends Service {
 
     /**
@@ -25,32 +17,16 @@ public interface HelloService extends Service {
 
 
     /**
-     * Example: curl -H "Content-Type: application/json" -X POST -d '{"message":
-     * "Hi"}' http://localhost:9000/api/hello/Alice
+     * Example: curl http://localhost:9000/api/hello-proxy/Alice
      */
-    ServiceCall<GreetingMessage, Done> useGreeting(String id);
-
-
-    /**
-     * This gets published to Kafka.
-     */
-    Topic<HelloEvent> helloEvents();
+    ServiceCall<NotUsed, String> helloProxy(String id);
 
     @Override
     default Descriptor descriptor() {
-        // @formatter:off
         return named("hello").withCalls(
                 pathCall("/api/hello/:id", this::hello),
-                pathCall("/api/hello/:id", this::useGreeting)
-        ).withTopics(
-                topic("hello-events", this::helloEvents)
-                        // Kafka partitions messages, messages within the same partition will
-                        // be delivered in order, to ensure that all messages for the same user
-                        // go to the same partition (and hence are delivered in order with respect
-                        // to that user), we configure a partition key strategy that extracts the
-                        // name as the partition key.
-                        .withProperty(KafkaProperties.partitionKeyStrategy(), HelloEvent::getName)
+                pathCall("/api/hello-proxy/:id", this::helloProxy)
         ).withAutoAcl(true);
-        // @formatter:on
     }
+
 }
--- lightbend-telemetry/lightbend-telemetry-java-mvn/hello-impl/src/main/java/com/lightbend/lagom/recipes/cinnamon/hello/impl/HelloModule.java
+++ lightbend-telemetry/lightbend-telemetry-java-mvn/hello-impl/src/main/java/com/lightbend/lagom/recipes/cinnamon/hello/impl/HelloModule.java
@@ -1,15 +1,31 @@
 package com.lightbend.lagom.recipes.cinnamon.hello.impl;
 
 import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.api.ServiceLocator;
+import com.lightbend.lagom.javadsl.client.ConfigurationServiceLocator;
 import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
 import com.lightbend.lagom.recipes.cinnamon.hello.api.HelloService;
+import com.typesafe.config.Config;
+import play.Environment;
 
-/**
- * The module that binds the HelloService so that it can be served.
- */
 public class HelloModule extends AbstractModule implements ServiceGuiceSupport {
+
+    private final Environment environment;
+    private final Config config;
+
+    public HelloModule(Environment environment, Config config) {
+        this.environment = environment;
+        this.config = config;
+    }
+
     @Override
     protected void configure() {
         bindService(HelloService.class, HelloServiceImpl.class);
+
+        // Only needed to allow the sample to run from test and dist with the config Service Locator
+        if (environment.isProd()) {
+            bind(ServiceLocator.class).to(ConfigurationServiceLocator.class);
+        }
     }
+
 }
