5412560fd49999c52deb0bff9caaa433728d2307
Tim Moore <tim.moore@lightbend.com>
2018-06-22 14:36:25 +0930

    Initial template project

---
+++ lightbend-telemetry/lightbend-telemetry-java-mvn/hello-impl/src/main/java/com/lightbend/lagom/recipes/cinnamon/hello/impl/HelloModule.java
@@ -0,0 +1,15 @@
+package com.lightbend.lagom.recipes.cinnamon.hello.impl;
+
+import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+import com.lightbend.lagom.recipes.cinnamon.hello.api.HelloService;
+
+/**
+ * The module that binds the HelloService so that it can be served.
+ */
+public class HelloModule extends AbstractModule implements ServiceGuiceSupport {
+  @Override
+  protected void configure() {
+    bindService(HelloService.class, HelloServiceImpl.class);
+  }
+}
