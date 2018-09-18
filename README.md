02cbb9427a5ecda8014547f421841a852ef9d41d
Sergey Morgunov <hostage@me.com>
2018-09-18 10:41:23 +0300

    Implement a java equivalent of the file upload recipe (#45)
    Fixes #43

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ See [`CONTRIBUTING.md`](CONTRIBUTING.md).
 * How do I use RDBMS read-sides with Cassandra write-sides? ([mixed persistence in java](mixed-persistence/mixed-persistence-java-sbt/README.md) or [mixed persistence in scala](mixed-persistence/mixed-persistence-scala-sbt/README.md))
 * How to create a stateless service in Lagom for Java that uses [Play's Internationalization Support](i18n/hello-i18n-java-mvn/README.md).
 * How do I manipulate Headers and Status Codes and test those cases?([HTTP header handling](./http-header-handling/http-header-handling-java-sbt/README.md))
-* How do I handle `multipart/form-data` file uploads? ([Scala example](./file-upload/file-upload-scala-sbt/README.md))
+* How do I handle `multipart/form-data` file uploads? ([Scala example](./file-upload/file-upload-scala-sbt/README.md), [Java example](./file-upload/file-upload-java-sbt/README.md))
 * How do I use a custom message serializer and response header to implement file downloads? ([Scala example](./file-download/file-download-scala-sbt/README.md))
 * How do I integrate Lightbend Telemetry (Cinnamon)? ([Java/Maven example](./lightbend-telemetry/lightbend-telemetry-java-mvn/README.md))
 * How do I configure the Circuit Breaker Panel for a Lagom Java application? ([Java/Maven example](./circuitbreakerpanel/circuitbreakerpanel-java-mvn/README.md))
---
+++ file-upload/file-upload-java-sbt/fileupload-api/src/main/java/com/example/fileupload/api/FileUploadService.java
@@ -0,0 +1,37 @@
+package com.example.fileupload.api;
+
+
+import com.lightbend.lagom.javadsl.api.Descriptor;
+import com.lightbend.lagom.javadsl.api.Service;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+
+import static com.lightbend.lagom.javadsl.api.Service.named;
+import static com.lightbend.lagom.javadsl.api.Service.pathCall;
+import static com.lightbend.lagom.javadsl.api.ServiceAcl.path;
+
+/**
+ * This Service represents your usual Lagom code. You will have many call heres.
+ * The particularity of this file resides on the ACLs declaration: we are manually
+ * creating the list of ACLs to include `/api/files` which is not a call handled
+ * by the Lagom Router but still a Call available in the FileUpload Application.
+ */
+public interface FileUploadService extends Service {
+
+    /**
+     * Invoke using:
+     * <code> curl -X POST -H "Content-Type: text/plain" -d  "hello world" http://localhost:9000/api/echo </code>
+     */
+    ServiceCall<String, String> uppercaseEcho();
+
+    @Override
+    default Descriptor descriptor() {
+        return named("fileupload")
+                .withCalls(
+                        pathCall("/api/echo", this::uppercaseEcho)
+                )
+                .withServiceAcls(
+                        path("/api/echo"),
+                        path("/api/files")
+                );
+    }
+}
