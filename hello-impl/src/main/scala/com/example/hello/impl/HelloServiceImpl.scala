d7fde9a804cdc1619682148f0a3a58ff3c8dae75
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-11-09 21:38:07 +0100

    Clean imports

--- hello-impl/src/main/scala/com/example/hello/impl/HelloServiceImpl.scala
+++ hello-impl/src/main/scala/com/example/hello/impl/HelloServiceImpl.scala
@@ -1,9 +1,7 @@
 package com.example.hello.impl
 
-import com.example.hello.api
 import com.example.hello.api.HelloService
 import com.lightbend.lagom.scaladsl.api.ServiceCall
-import com.lightbend.lagom.scaladsl.api.broker.Topic
 
 import scala.concurrent.Future
 
