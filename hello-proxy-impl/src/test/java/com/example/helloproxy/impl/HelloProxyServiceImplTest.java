bd51db7dd41b9a66ea77afb2f718408447edd486
Ignasi Marimon-Clos <ignasi@lightbend.com>
2018-11-13 00:11:25 +0100

    Introduces test with gRPC stub.

--- hello-proxy-impl/src/test/java/com/example/helloproxy/impl/HelloProxyServiceImplTest.java
+++ hello-proxy-impl/src/test/java/com/example/helloproxy/impl/HelloProxyServiceImplTest.java
@@ -1,35 +1,33 @@
 package com.example.helloproxy.impl;
 
+import akka.Done;
 import akka.NotUsed;
-import akka.actor.ActorSystem;
-import akka.grpc.GrpcClientSettings;
-import akka.stream.Materializer;
+import akka.grpc.javadsl.AkkaGrpcClient;
 import com.example.hello.api.HelloService;
+import com.example.helloproxy.api.HelloProxyService;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 import com.lightbend.lagom.javadsl.testkit.ServiceTest;
 import example.myapp.helloworld.grpc.AkkaGrpcClientModule;
 import example.myapp.helloworld.grpc.GreeterServiceClient;
+import example.myapp.helloworld.grpc.HelloReply;
+import example.myapp.helloworld.grpc.HelloRequest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import scala.concurrent.ExecutionContext;
 
-import javax.inject.Inject;
-import javax.inject.Provider;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-import static com.lightbend.lagom.javadsl.testkit.ServiceTest.startServer;
-import static com.lightbend.lagom.javadsl.testkit.ServiceTest.defaultSetup;
-import static com.lightbend.lagom.javadsl.testkit.ServiceTest.withServer;
-import static com.lightbend.lagom.javadsl.testkit.ServiceTest.bind;
+import static com.lightbend.lagom.javadsl.testkit.ServiceTest.*;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
 
 public class HelloProxyServiceImplTest {
 
     private static ServiceTest.TestServer server;
+    private static    HelloProxyService proxyServiceClient ;
 
     @BeforeClass
     public static void setUp() {
@@ -40,9 +38,10 @@ public class HelloProxyServiceImplTest {
                 builder
                     .disable(AkkaGrpcClientModule.class)
                     .overrides(bind(HelloService.class).to(StubHelloService.class))
-                    .overrides(bind(GreeterServiceClient.class).toProvider(GreeterServiceClientStubProvider.class))
+                    .overrides(bind(GreeterServiceClient.class).to(GreeterServiceClientStub.class))
             );
         server = startServer(setup);
+        proxyServiceClient = server.client(HelloProxyService.class);
     }
 
     @AfterClass
@@ -55,10 +54,15 @@ public class HelloProxyServiceImplTest {
 
     @Test
     public void helloProxyShouldRoundtripHttpRequests() throws InterruptedException, ExecutionException, TimeoutException {
-        HelloService proxyServiceClient = server.client(HelloService.class);
-        String msg = proxyServiceClient.hello("Alice").invoke()
+        String msg = proxyServiceClient.proxyViaHttp("Alice").invoke()
             .toCompletableFuture().get(5, SECONDS);
         assertEquals("Hello Alice", msg);
+    }
+    @Test
+    public void helloProxyShouldRoundtripGrpcRequests() throws InterruptedException, ExecutionException, TimeoutException {
+        String msg = proxyServiceClient.proxyViaGrpc("Steve").invoke()
+            .toCompletableFuture().get(5, SECONDS);
+        assertEquals("Hi Steve (gRPC)", msg);
     }
 
     // ---------------------------------------------------------------------------------
@@ -70,20 +74,29 @@ public class HelloProxyServiceImplTest {
         }
     }
 
-    public static class GreeterServiceClientStubProvider implements Provider<GreeterServiceClient> {
 
-        @Inject
-        private Materializer mat;
-        @Inject
-        private ExecutionContext ec;
-        @Inject
-        private ActorSystem actorSys;
+    public static class GreeterServiceClientStub extends GreeterServiceClient implements StubbedAkkaGrpcClient {
+        @Override
+        public CompletionStage<HelloReply> sayHello(HelloRequest in) {
+            HelloReply reply = HelloReply.newBuilder()
+                .setMessage("Hi " + in.getName() + " (gRPC)")
+                .build();
+            return CompletableFuture.completedFuture(reply);
+        }
+    }
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+    public interface StubbedAkkaGrpcClient extends AkkaGrpcClient{
+        @Override
+        default CompletionStage<Done> close() {
+            return null;
+        }
 
         @Override
-        public GreeterServiceClient get() {
-            GrpcClientSettings settings =
-                GrpcClientSettings.connectToServiceAt("localhost", 2525, actorSys);
-            return GreeterServiceClient.create(settings, mat, ec);
+        default CompletionStage<Done> closed() {
+            return null;
         }
     }
 
