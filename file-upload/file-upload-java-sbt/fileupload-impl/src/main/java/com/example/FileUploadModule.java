418cc58ee3c9e030433b730e1d24fe7aebd05b3e
Renato Cavalcanti <renato@cavalcanti.be>
2019-02-28 22:20:24 +0100

    File upload using new additional router API (Java)

--- file-upload/file-upload-java-sbt/fileupload-impl/src/main/java/com/example/FileUploadModule.java
+++ file-upload/file-upload-java-sbt/fileupload-impl/src/main/java/com/example/FileUploadModule.java
@@ -4,15 +4,13 @@ import com.example.fileupload.api.FileUploadService;
 import com.example.fileupload.impl.FileUploadServiceImpl;
 import com.google.inject.AbstractModule;
 import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
-import router.Routes;
 
 public class FileUploadModule extends AbstractModule implements ServiceGuiceSupport {
-
     @Override
     protected void configure() {
         bindService(
                 FileUploadService.class, FileUploadServiceImpl.class,
-                additionalRouter(Routes.class)
+                additionalRouter(FileUploadRouter.class)
         );
     }
 }
