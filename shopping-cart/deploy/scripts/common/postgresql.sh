5653ad254c035596f9d6299472c0e5c748dd6da2
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-05-16 13:55:16 +0200

    Refactor: extact several deployment steps to functions

---
+++ shopping-cart/deploy/scripts/common/postgresql.sh
@@ -0,0 +1,64 @@
+## TODO: fix this --> schemas/shopping-cart.sql is not found
+## TODO: parameterize this script
+#    secret     postgresql-admin-password
+#    secret     postgres-shopping-cart
+#    db_name    shopping_cart_database
+#    db_user    shopping_cart_user
+#    db_script  schemas/shopping-cart.sql
+
+
+installPostgres() {
+  echo "Installing Postgres"
+
+  ## TODO: use `--name=some-name-psql` so we can deploy multiple PG's on a single openshift project
+  ## create a new app using postgresql
+  oc new-app postgresql
+
+  ## create new admin pwd 
+  oc create secret generic postgresql-admin-password --from-literal=password="$(openssl rand -base64 24)"
+
+  ## patch app to use new admin pwd
+  oc patch deploymentconfig postgresql --patch '{"spec": {"template": {"spec": {"containers": [
+    {"name": "postgresql", "env": [
+      {"name": "POSTGRESQL_ADMIN_PASSWORD", "valueFrom":
+        {"secretKeyRef": {"name": "postgresql-admin-password", "key": "password"}}
+      }
+    ]}
+  ]}}}}'
+  ## Wait for postgresql pods/app to be ready
+  waitForApp app=postgresql 1
+}
+
+
+createDatabase() {
+  DATABASE_SCHEMA=$1
+
+  ## create user pwd 
+  oc create secret generic postgres-shopping-cart --from-literal=username=shopping_cart_user --from-literal=password="$(openssl rand -base64 24)"
+
+  ## Forward PGSQL port to conenct remotely
+  oc port-forward svc/postgresql 15432:5432 &
+  echo Sleeping for 10 seconds while port forward is established...
+  sleep 10
+
+  #create-ddl
+  psql -h localhost -p 15432 -U postgres <<DDL
+CREATE DATABASE shopping_cart_database;
+REVOKE CONNECT ON DATABASE shopping_cart_database FROM PUBLIC;
+CREATE USER shopping_cart_user WITH PASSWORD '$(oc get secret postgres-shopping-cart -o jsonpath='{.data.password}' | base64 --decode)';
+GRANT CONNECT ON DATABASE shopping_cart_database TO shopping_cart_user;
+
+\connect shopping_cart_database;
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+GRANT USAGE ON SCHEMA public TO shopping_cart_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+  GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO shopping_cart_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+  GRANT SELECT, USAGE ON SEQUENCES TO shopping_cart_user;
+
+\include $DATABASE_SCHEMA;
+DDL
+
+  ## Close the port-forward
+  kill %1
+}
--- shopping-cart/deploy/scripts/deploy.sh
+++ shopping-cart/deploy/scripts/deploy.sh
@@ -3,24 +3,21 @@
 
 # Recognize the environment
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+COMMON_SCRIPTS_DIR=$SCRIPTS_DIR/common
 DEPLOY_DIR=$SCRIPTS_DIR/..
 BASE_DIR=$DEPLOY_DIR/..
-SHOPPING_CART_SCALA__DIR=$BASE_DIR/shopping-cart-scala
+SHOPPING_CART_SCALA_DIR=$BASE_DIR/shopping-cart-scala
 
 
 # Load some helping functions
-. $SCRIPTS_DIR/waitForApp.sh
+. $COMMON_SCRIPTS_DIR/deployment-tools.sh
+. $COMMON_SCRIPTS_DIR/postgresql.sh
 
 # Setup env vars for deployment
 export OPENSHIFT_SERVER=centralpark2.lightbend.com
-# OPENSHIFT_PROJECT and NAMESPACE will be used indistinctly in this scripts
-export OPENSHIFT_PROJECT=shopping-cart-lagom
-export NAMESPACE=$OPENSHIFT_PROJECT
-export IMAGE_SHOPPING_CART=shopping-cart
-export IMAGE_INVENTORY=inventory
-
-export DOCKER_REGISTRY_SERVER=docker-registry-default.centralpark2.lightbend.com
-export DOCKER_REGISTRY=$DOCKER_REGISTRY_SERVER/$OPENSHIFT_PROJECT
+export NAMESPACE=shopping-cart-lagom
+export DOCKER_REGISTRY=docker-registry-default.centralpark2.lightbend.com
+export DOCKER_REPOSITORY=$DOCKER_REGISTRY/$NAMESPACE
 
 
 echo "Attempting login to Openshift cluster (this will fail on PR builds)"
@@ -28,57 +25,38 @@ if [ -z ${CP2_PLAY_PASSWORD+x} ]; then echo "CP2_PLAY_PASSWORD is unset."; else 
 oc login https://$OPENSHIFT_SERVER --username=play-team --password=$CP2_PLAY_PASSWORD  || exit 1
 
 
-oc delete project $OPENSHIFT_PROJECT
+deleteNamespace $NAMESPACE
+createNamespace $NAMESPACE
 
 build() {
-        cd $SHOPPING_CART_SCALA__DIR
-        sbt docker:publishLocal 
+    cd $SHOPPING_CART_SCALA_DIR
+    sbt clean docker:publishLocal 
 
-        ## once the images are published, we can inspect the generated files to discover the tag
-        ## The following works by looking for a file named something like `target/scala-2.12/shopping-cart-scala_2.12-353-0e34faa0.pom`
-        ## It then reverses the string: mop.0aaf43e0-353-21.2_alacs-trac-gnippohs/21.2-alacs/tegrat
-        ## Cuts the few bits using '.' and '-' and reverses again: "353-0e34faa0"
-        export IMAGE_TAG=`find target/scala-2.12/sho* | grep pom | grep shopping-cart | rev | cut -d'-' -f1,2 | cut -d'.' -f2|rev`
-        cd $SCRIPTS_DIR
+    ## once the images are published, we can inspect the generated files to discover the tag
+    ## The following works by looking for a file named something like `target/scala-2.12/shopping-cart-scala_2.12-353-0e34faa0.pom`
+    ## It then reverses the string: mop.0aaf43e0-353-21.2_alacs-trac-gnippohs/21.2-alacs/tegrat
+    ## Cuts a few bits using '.' and '-' and reverses again: "353-0e34faa0"
+    export IMAGE_TAG=`find target/scala-2.12/sho* | grep pom | grep shopping-cart | rev | cut -d'-' -f1,2 | cut -d'.' -f2|rev`
+    cd $SCRIPTS_DIR
+    echo " - - - "
+    echo "Built images shopping-cart:$IMAGE_TAG"
+    echo "Built images inventory:$IMAGE_TAG"
+    echo " - - - "
 }
 
 build
 
-echo "Built image $IMAGE_SHOPPING_CART:$IMAGE_TAG"
+## TODO parameterize these functions
+installPostgres
+createDatabase $SHOPPING_CART_SCALA_DIR/schemas/shopping-cart.sql
 
 
-oc new-project $OPENSHIFT_PROJECT
+dockerLogin $DOCKER_REGISTRY
 
-## Must not try to create a PGsql app too son after creating the project in line 22. Damn OC
-sleep 5
-. postgresql.sh
+pushImage $DOCKER_REPOSITORY "shopping-cart" $IMAGE_TAG
+pushImage $DOCKER_REPOSITORY "inventory" $IMAGE_TAG
 
-oc whoami -t | docker login -u unused --password-stdin $DOCKER_REGISTRY_SERVER
+setupRbac $NAMESPACE ../specs/common/rbac.yaml
 
-pushImage() {
-    REGISTRY=$1
-    shift
-    NAME=$1
-    shift
-    TAG=$1
-    shift
-
-    echo "Tagging image: $NAME:$TAG $DOCKER_REGISTRY/$NAME:$TAG"
-    echo "Tagging image: $NAME:latest $DOCKER_REGISTRY/$NAME:latest"
-    docker tag $NAME:$TAG $REGISTRY/$NAME:$TAG
-    docker tag $NAME:$TAG $REGISTRY/$NAME:latest
-    docker push $REGISTRY/$NAME:$TAG
-    docker push $REGISTRY/$NAME:latest
-}
-
-pushImage $DOCKER_REGISTRY "shopping-cart" $IMAGE_TAG
-
-
-## final setups before deploying
-# a secret for the shopping-cart app 
-oc create secret generic shopping-cart-application-secret --from-literal=secret="$(openssl rand -base64 48)"
-# tuning openshift's image lookup
-oc set image-lookup shopping-cart
-
-## DEPLOYOLO!
-sed -e "s/myproject/shopping-cart-lagom/g" ../specs/shopping-cart.yaml | oc apply -f - 
+deploy shopping-cart ../specs/shopping-cart.yaml
+deploy inventory ../specs/inventory.yaml
