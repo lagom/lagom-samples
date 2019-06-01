a8416ed2d6712e030892bdce07b6078f0f9e1c1f
Marcos Pereira <marcos.silva@gmail.com>
2019-05-31 23:08:14 -0400

    Fix problem with Jackson deserialization

--- mixed-persistence/mixed-persistence-java-sbt/hello-api/src/main/java/com/lightbend/lagom/samples/mixedpersistence/hello/api/UserGreeting.java
+++ mixed-persistence/mixed-persistence-java-sbt/hello-api/src/main/java/com/lightbend/lagom/samples/mixedpersistence/hello/api/UserGreeting.java
@@ -3,8 +3,19 @@ package com.lightbend.lagom.samples.mixedpersistence.hello.api;
 import lombok.NonNull;
 import lombok.Value;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.base.Preconditions;
+
 @Value
+@JsonDeserialize
 public class UserGreeting {
     @NonNull String id;
     @NonNull String message;
+
+    @JsonCreator
+    public UserGreeting(String id, String message) {
+        this.id = Preconditions.checkNotNull(id, "id field MUST not be null");
+        this.message = Preconditions.checkNotNull(message, "message field MUST not be null");
+    }
 }
--- mixed-persistence/mixed-persistence-java-sbt/hello-impl/src/main/java/com/lightbend/lagom/samples/mixedpersistence/hello/impl/entity/HelloCommand.java
+++ mixed-persistence/mixed-persistence-java-sbt/hello-impl/src/main/java/com/lightbend/lagom/samples/mixedpersistence/hello/impl/entity/HelloCommand.java
@@ -2,10 +2,16 @@ package com.lightbend.lagom.samples.mixedpersistence.hello.impl.entity;
 
 import akka.Done;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntity;
+import com.lightbend.lagom.samples.mixedpersistence.hello.api.UserGreeting;
 import com.lightbend.lagom.serialization.CompressedJsonable;
 import com.lightbend.lagom.serialization.Jsonable;
 import lombok.NonNull;
 import lombok.Value;
+
+import com.google.common.base.Preconditions;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 /**
  * This interface defines all the commands that the HelloEntity supports.
@@ -23,8 +29,14 @@ public interface HelloCommand extends Jsonable {
      * when all the events emitted by this command are successfully persisted.
      */
     @Value
+    @JsonDeserialize
     final class UseGreetingMessage implements HelloCommand, CompressedJsonable, PersistentEntity.ReplyType<Done> {
         @NonNull String message;
+
+        @JsonCreator
+        public UseGreetingMessage(String message) {
+            this.message = Preconditions.checkNotNull(message, "message must NOT be null");
+        }
     }
 
     /**
@@ -34,8 +46,14 @@ public interface HelloCommand extends Jsonable {
      * person.
      */
     @Value
+    @JsonDeserialize
     final class Hello implements HelloCommand, PersistentEntity.ReplyType<String> {
         @NonNull String name;
+
+        @JsonCreator
+        public Hello(String name) {
+            this.name = Preconditions.checkNotNull(name, "name must NOT be null");
+        }
     }
 
 }
