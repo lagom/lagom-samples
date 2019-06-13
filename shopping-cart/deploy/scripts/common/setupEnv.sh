ecd2d8c533a095ea3f141a95f1bd4f96195267e8
Tim Moore <tim.moore@lightbend.com>
2019-06-13 12:34:15 +0930

    Translate dots in branch name to hyphens
    This fixes the broken build on the 1.5.x branch.

--- shopping-cart/deploy/scripts/common/setupEnv.sh
+++ shopping-cart/deploy/scripts/common/setupEnv.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+## The ID will be the PR number, the branch name on Travis or `local`
+if [[ -n "${TRAVIS_PULL_REQUEST}" && "${TRAVIS_PULL_REQUEST}" != false ]]
+then
+    ID="${TRAVIS_PULL_REQUEST}" # Pull request number
+elif [[ -n "${TRAVIS_BRANCH}" ]]
+then
+    ID="${TRAVIS_BRANCH//./-}" # Branch name, with dots replaced by hyphens
+else
+    ID=local
+fi
+export NAMESPACE=lagom-$CODE_VARIANT-$BUILD_TOOL-$USER-$ID
+
 # Setup env vars for deployment
 export OPENSHIFT_SERVER=centralpark2.lightbend.com
 ## Must not use `DOCKER_REGISTRY` as the name of the ENV VAR since it clashes 
