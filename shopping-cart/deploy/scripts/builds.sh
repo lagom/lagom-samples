c47113ae5ad09bc0586196df76bd259905c9e78b
Dale Wijnand <dale.wijnand@gmail.com>
2019-10-17 13:00:03 +0100

    Avoid masking return values
        In shopping-cart/deploy/scripts/builds.sh line 7:
            export IMAGE_TAG=`docker images  |grep shopping-cart|grep -v latest | awk -F\  '{print $2}'| head -1`
                   ^-------^ SC2155: Declare and assign separately to avoid masking return values.

--- shopping-cart/deploy/scripts/builds.sh
+++ shopping-cart/deploy/scripts/builds.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 setTag() {
-    ## Assuming `docker images` sorts in reverse chronological order (and probably it'll be pointing to an 
-    ## empty repository anyway) we list all images, filter by non-latest and filter by name. Finally, take 
+    ## Assuming `docker images` sorts in reverse chronological order (and probably it'll be pointing to an
+    ## empty repository anyway) we list all images, filter by non-latest and filter by name. Finally, take
     ## only the first match (which is the recently built).
-    export IMAGE_TAG=`docker images  |grep shopping-cart|grep -v latest | awk -F\  '{print $2}'| head -1`
+    IMAGE_TAG=`docker images  |grep shopping-cart|grep -v latest | awk -F\  '{print $2}'| head -1`
+    export IMAGE_TAG
     echo " - - - "
     echo "Built images shopping-cart:$IMAGE_TAG"
     echo "Built images inventory:$IMAGE_TAG"
