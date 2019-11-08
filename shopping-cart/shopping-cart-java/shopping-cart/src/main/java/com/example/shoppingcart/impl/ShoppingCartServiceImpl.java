578a911e71096910ec7f35904be5bdc90868d73f
Renato Cavalcanti <renato@cavalcanti.be>
2019-11-08 18:09:51 +0100

    Align model names with Lagom documentation

--- shopping-cart/shopping-cart-java/shopping-cart/src/main/java/com/example/shoppingcart/impl/ShoppingCartServiceImpl.java
+++ shopping-cart/shopping-cart-java/shopping-cart/src/main/java/com/example/shoppingcart/impl/ShoppingCartServiceImpl.java
@@ -42,14 +42,14 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         // register entity on shard
         this.clusterSharing.init(
                 Entity.of(
-                        ShoppingCart.ENTITY_TYPE_KEY,
-                        ShoppingCart::create
+                        ShoppingCartEntity.ENTITY_TYPE_KEY,
+                        ShoppingCartEntity::create
                 )
         );
     }
 
-    private EntityRef<ShoppingCart.Command> entityRef(String id) {
-        return clusterSharing.entityRefFor(ShoppingCart.ENTITY_TYPE_KEY, id);
+    private EntityRef<ShoppingCartEntity.Command> entityRef(String id) {
+        return clusterSharing.entityRefFor(ShoppingCartEntity.ENTITY_TYPE_KEY, id);
     }
 
     private final Duration askTimeout = Duration.ofSeconds(5);
@@ -58,7 +58,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public ServiceCall<NotUsed, ShoppingCartView> get(String id) {
         return request ->
                 entityRef(id)
-                        .ask(ShoppingCart.Get::new, askTimeout)
+                        .ask(ShoppingCartEntity.Get::new, askTimeout)
                         .thenApply(summary -> asShoppingCartView(id, summary));
     }
 
@@ -76,8 +76,8 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public ServiceCall<ShoppingCartItem, Done> addItem(String cartId) {
         return item ->
                 entityRef(cartId)
-                .<ShoppingCart.Confirmation>ask(replyTo ->
-                        new ShoppingCart.AddItem(item.getItemId(), item.getQuantity(), replyTo), askTimeout)
+                .<ShoppingCartEntity.Confirmation>ask(replyTo ->
+                        new ShoppingCartEntity.AddItem(item.getItemId(), item.getQuantity(), replyTo), askTimeout)
                 .thenApply(this::handleConfirmation)
                 .thenApply(accepted -> Done.getInstance());
     }
@@ -86,8 +86,8 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public ServiceCall<NotUsed, ShoppingCartView> removeItem(String cartId, String itemId) {
         return request ->
             entityRef(cartId)
-                .<ShoppingCart.Confirmation>ask(replyTo ->
-                    new ShoppingCart.RemoveItem(itemId, replyTo), askTimeout)
+                .<ShoppingCartEntity.Confirmation>ask(replyTo ->
+                    new ShoppingCartEntity.RemoveItem(itemId, replyTo), askTimeout)
                     .thenApply(this::handleConfirmation)
                     .thenApply(accepted -> asShoppingCartView(cartId, accepted.getSummary()));
     }
@@ -96,15 +96,15 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public ServiceCall<Quantity, ShoppingCartView> adjustItemQuantity(String cartId, String itemId) {
         return quantity ->
             entityRef(cartId)
-                .<ShoppingCart.Confirmation>ask(replyTo ->
-                        new ShoppingCart.AdjustItemQuantity(itemId, quantity.getQuantity(), replyTo), askTimeout)
+                .<ShoppingCartEntity.Confirmation>ask(replyTo ->
+                        new ShoppingCartEntity.AdjustItemQuantity(itemId, quantity.getQuantity(), replyTo), askTimeout)
                 .thenApply(this::handleConfirmation)
                 .thenApply(accepted -> asShoppingCartView(cartId, accepted.getSummary()));
     }
 
     @Override
     public ServiceCall<NotUsed, Done> checkout(String cartId) {
-        return request -> entityRef(cartId).ask(ShoppingCart.Checkout::new, askTimeout)
+        return request -> entityRef(cartId).ask(ShoppingCartEntity.Checkout::new, askTimeout)
                 .thenApply(this::handleConfirmation)
                 .thenApply(accepted -> Done.getInstance());
     }
@@ -112,16 +112,16 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public Topic<ShoppingCartView> shoppingCartTopic() {
         // We want to publish all the shards of the shopping cart events
-        return TopicProducer.taggedStreamWithOffset(ShoppingCart.Event.TAG.allTags(), (tag, offset) ->
+        return TopicProducer.taggedStreamWithOffset(ShoppingCartEntity.Event.TAG.allTags(), (tag, offset) ->
                 // Load the event stream for the passed in shard tag
                 persistentEntityRegistry.eventStream(tag, offset)
                         // We only want to publish checkout events
-                        .filter(pair -> pair.first() instanceof ShoppingCart.CheckedOut)
+                        .filter(pair -> pair.first() instanceof ShoppingCartEntity.CheckedOut)
                         // Now we want to convert from the persisted event to the published event.
                         // To do this, we need to load the current shopping cart state.
                         .mapAsync(4, eventAndOffset -> {
-                            ShoppingCart.CheckedOut checkedOut = (ShoppingCart.CheckedOut) eventAndOffset.first();
-                            return entityRef(checkedOut.getShoppingCartId()).ask(ShoppingCart.Get::new, askTimeout)
+                            ShoppingCartEntity.CheckedOut checkedOut = (ShoppingCartEntity.CheckedOut) eventAndOffset.first();
+                            return entityRef(checkedOut.getShoppingCartId()).ask(ShoppingCartEntity.Get::new, askTimeout)
                                     .thenApply(summary -> Pair.create(asShoppingCartView(checkedOut.getShoppingCartId(), summary),
                                             eventAndOffset.second()));
                         }));
@@ -133,17 +133,17 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
      *
      * @throws BadRequest if Confirmation is a Rejected
      */
-    private ShoppingCart.Accepted handleConfirmation(ShoppingCart.Confirmation confirmation) {
-        if (confirmation instanceof ShoppingCart.Accepted) {
-            ShoppingCart.Accepted accepted = (ShoppingCart.Accepted) confirmation;
+    private ShoppingCartEntity.Accepted handleConfirmation(ShoppingCartEntity.Confirmation confirmation) {
+        if (confirmation instanceof ShoppingCartEntity.Accepted) {
+            ShoppingCartEntity.Accepted accepted = (ShoppingCartEntity.Accepted) confirmation;
             return accepted;
         }
 
-        ShoppingCart.Rejected rejected = (ShoppingCart.Rejected) confirmation;
+        ShoppingCartEntity.Rejected rejected = (ShoppingCartEntity.Rejected) confirmation;
         throw new BadRequest(rejected.getReason());
     }
 
-    private ShoppingCartView asShoppingCartView(String id, ShoppingCart.Summary summary) {
+    private ShoppingCartView asShoppingCartView(String id, ShoppingCartEntity.Summary summary) {
         List<ShoppingCartItem> items = new ArrayList<>();
         for (Map.Entry<String, Integer> item : summary.getItems().entrySet()) {
             items.add(new ShoppingCartItem(item.getKey(), item.getValue()));
